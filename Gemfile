2ba266f47f4c90a070dd2248bf4234ef11f1d62c
chrihill627 <yorkshiretable@gmail.com>
2014-01-20 19:41:53 +0000

    first producton version

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0.beta1'
@@ -49,6 +49,7 @@ group :test, :development do
 
 end
 
+gem 'rails_12factor', group: :production
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
-  has_attached_file :image, :styles => { thumb: "300x300>" }
+  has_attached_file :image, 
+  :styles => { thumb: "300x300>" },
+  storage: :s3,
+  s3_credentials: {
+    access_key_id: 'AKIAJIT2TOCWJOPENM7Q',
+    secret_access_key: Rails.application.secrets.s3_secret
+  },
+  bucket: 'christagram_development'
+
 end
