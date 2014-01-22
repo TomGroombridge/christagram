9529f586f552f102b9224cb844bb3b4ad26b021d
chrihill627 <yorkshiretable@gmail.com>
2014-01-22 11:36:34 +0000

    styled pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,25 @@
 class Post < ActiveRecord::Base
   has_attached_file :image, 
-  :styles => { thumb: " ", small: "150x150>" },
-  :convert_options => { 
-  :thumb => "-gravity Center -crop 500x500+0+0 +repage -resize 300x300^" },
-  :default_style => :thumb,
+  :styles => { thumb: "300x300^", small: "150x150>" },
+
+  #resize the image first and then crop????
+
+  # :convert_options => { 
+  # :thumb => "-gravity Center -crop 1600x1000+0+0 +repage -resize 300x300^" },
+  # :default_style => :thumb,
   storage: :s3,
   s3_credentials: {
     access_key_id: 'AKIAJIT2TOCWJOPENM7Q',
     secret_access_key: Rails.application.secrets.s3_secret
   },
   bucket: 'christagram_development'
+
+  # def resize     
+  #  geo = Paperclip::Geometry.from_file(image.to_file(:original))
+  #  height = (geo.width.to_i * 60)/100
+  #  width = geo.width     
+  #  "#{width.round}x#{height.round}!"    
+  # end  
 
   belongs_to :user
 
