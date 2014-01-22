a78986ebca7368004dce643253ebc5e0eafc2165
TomGroombridge <groombridgetom@gmail.com>
2014-01-22 18:07:49 +0000

    refactored code

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ActiveRecord::Base
+
+	before_validation :format_name
+
   has_and_belongs_to_many :posts
 
   def to_s
@@ -9,4 +12,15 @@ class Tag < ActiveRecord::Base
     name
   end
 
+  def self.find_or_create_from_tag_names(tag_names)
+  	 tag_names.downcase.split(/,\s?/).map do |tag_name|
+      find_or_create_by(name: tag_name)
+    end
+  end
+
+private
+
+	def format_name
+		self.name = name.downcase.gsub(/[^a-z]/, '')
+	end  
 end
