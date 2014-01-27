3d07f21dddc805d9922fc48d1d1577d13be4d97d
TomGroombridge <groombridgetom@gmail.com>
2014-01-27 18:25:59 +0000

    changes committed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,6 @@
 //= require jquery.ui.all  
 //= require inputosaurus
 //= require twitter/bootstrap
+//= $('#post_tag_names')
 //= require turbolinks
 //= require_tree .
-//= $('#post_tag_names')
