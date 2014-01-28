6a790507707c09371019fd79018b6943c443676f
TomGroombridge <groombridgetom@gmail.com>
2014-01-28 16:02:19 +0000

    sign in for commenting

--- spec/features/addin_photos_spec.rb
+++ spec/features/addin_photos_spec.rb
@@ -4,10 +4,10 @@ describe 'adding a photo' do
 
     it 'should be able to upload a photo' do
       visit 'posts/new'
-      fill_in 'Title', with: 'My photo'
-      fill_in 'Content', with: 'testing adding a pic'
+      fill_in 'title', with: 'My photo'
+      fill_in 'content', with: 'testing adding a pic'
       attach_file 'Image', Rails.root.join('spec/images/plane.jpg')
-      click_button 'Create Post'
+      click_button 'Post'
 
       expect(current_path).to eq '/posts'
       expect(page).to have_css 'img.christagram-upload'
--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -12,9 +12,9 @@ describe 'editing' do
   it 'should change the post' do
     visit '/posts'
     click_link 'edit'
-    fill_in 'Title', with:'goodbye world'
-    fill_in 'Content', with:'see you later'
-    click_button 'Update Post'
+    fill_in 'title', with:'goodbye world'
+    fill_in 'content', with:'see you later'
+    click_button 'Post'
     expect(page).to have_content 'goodbye world'
     expect(page).to have_content 'see you later'
   end
